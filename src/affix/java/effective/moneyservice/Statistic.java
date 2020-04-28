d9a11f39f77192d3d6e2356cdd20a5496b376e61
Niklas Svensson <Niklas.Svensson@yaelev.se>
2020-04-28 10:03:47 +0200

    Resolved issue after merge

--- src/affix/java/effective/moneyservice/Statistic.java
+++ src/affix/java/effective/moneyservice/Statistic.java
@@ -27,46 +27,46 @@ public class Statistic {
 		this.currencyCodes = currencyCodes;
 		this.siteName = siteName;
 	}
-  
-  /**
+
+	/**
 	 * @return the currencyCodes
 	 */
 	public List<String> getCurrencyCodes() {
 		return currencyCodes;
 	}
-  
-  /**
+
+	/**
 	 * @return the transactions
 	 */
 	public List<Transaction> getTransactions() {
 		return transactions;
-  }
-  
-  /**
+	}
+
+	/**
 	 * @return the siteName
 	 */
 	public String getSiteName() {
 		return siteName;
 	}
-	
+
 	/**
 	 * Get the total amount for each currency in the chosen reference currency
 	 * @param filteredDate
 	 * @return a map with an amount for each currency in reference currency
 	 */
 	public Map<String, Integer> getTotalAmount(String filteredDate) {
-		
+
 		HQApp.currencyMap = HQApp.readCurrencyConfigFile(String.format("ExchangeRates/CurrencyConfig_%s.txt", filteredDate));
 		Map<String, Integer> hm = new HashMap<String, Integer>();
-				
+
 		for(Transaction transaction : transactions) {
 			hm.put( transaction.getCurrencyCode(), (int) Math.round((double) transaction.getAmount() / HQApp.currencyMap.get(transaction.getCurrencyCode()).getExchangeRate()) );
 		}
-		
+
 		Set<Map.Entry<String, Integer>> eset = hm.entrySet();
-		
+
 		Map<String, Integer> resultMap = eset.stream().collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
-		
+
 		return resultMap;
 	}
 
@@ -79,16 +79,16 @@ public class Statistic {
 
 		HQApp.currencyMap = HQApp.readCurrencyConfigFile(String.format("ExchangeRates/CurrencyConfig_%s.txt", filteredDate));
 		Map<String, Integer> hm = new HashMap<String, Integer>();
-				
+
 		for(Transaction transaction : transactions) {
 			if (transaction.getMode().equals(TransactionMode.BUY))
-			hm.put( transaction.getCurrencyCode(), (int) Math.round((double) transaction.getAmount() / HQApp.currencyMap.get(transaction.getCurrencyCode()).getExchangeRate()) );
+				hm.put( transaction.getCurrencyCode(), (int) Math.round((double) transaction.getAmount() / HQApp.currencyMap.get(transaction.getCurrencyCode()).getExchangeRate()) );
 		}
-		
+
 		Set<Map.Entry<String, Integer>> eset = hm.entrySet();
-		
+
 		Map<String, Integer> resultMap = eset.stream().collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
-		
+
 		return resultMap;
 	}
 
@@ -101,19 +101,22 @@ public class Statistic {
 
 		HQApp.currencyMap = HQApp.readCurrencyConfigFile(String.format("ExchangeRates/CurrencyConfig_%s.txt", filteredDate));
 		Map<String, Integer> hm = new HashMap<String, Integer>();
-				
+
 		for(Transaction transaction : transactions) {
 			if (transaction.getMode().equals(TransactionMode.SELL))
-			hm.put( transaction.getCurrencyCode(), (int) Math.round((double) transaction.getAmount() / HQApp.currencyMap.get(transaction.getCurrencyCode()).getExchangeRate()) );
+				hm.put( transaction.getCurrencyCode(), (int) Math.round((double) transaction.getAmount() / HQApp.currencyMap.get(transaction.getCurrencyCode()).getExchangeRate()) );
 		}
-		
+
 		Set<Map.Entry<String, Integer>> eset = hm.entrySet();
-		
+
 		Map<String, Integer> resultMap = eset.stream().collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
-	
+		
+		return resultMap;
+	}	
+
 	public Map<String, Integer> getTotalTransactions() {
 		Map<String, Integer> resultMap = new HashMap<>();
-		
+
 		for (Iterator<String> iter = currencyCodes.iterator(); iter.hasNext(); ) {
 			String code = iter.next();
 			long count = transactions.stream()
@@ -121,77 +124,77 @@ public class Statistic {
 					.count();
 
 			Integer result = Math.toIntExact(count);
-			
+
 			resultMap.putIfAbsent(code, result);
 		}
 		return resultMap;
 	}
 
 
-		public Map<String, Integer> getDiffCurrency() {
-			Map<String, Integer> resultMap = new HashMap<>();
-			int difference = 0;
-			for (Iterator<String> iter = currencyCodes.iterator(); iter.hasNext(); ) {
-				String code = iter.next();
-				
-				Integer buyAmount = transactions.stream()
-						.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
-						.filter(t -> t.getMode().equals(TransactionMode.BUY))
-						.map(t -> t.getAmount())
-						.reduce(0, Integer::sum);
-				
-				Integer sellAmount = transactions.stream()
-						.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
-						.filter(t -> t.getMode().equals(TransactionMode.SELL))
-						.map(t -> t.getAmount())
-						.reduce(0, Integer::sum);
-				
-				difference = buyAmount - sellAmount;
-				resultMap.putIfAbsent(code, difference);
-			}
-			return resultMap;
-		}
-		
-		public Map<String, Integer> getProfit(String filteredDate) {
-			
-			HQApp.currencyMap = HQApp.readCurrencyConfigFile(String.format("ExchangeRates/CurrencyConfig_%s.txt", filteredDate));
-			Map<String, Integer> resultMap = new HashMap<>();
-			
-			for (Iterator<String> iter = currencyCodes.iterator(); iter.hasNext(); ) {
-				
-				String code = iter.next();
-				Currency temp = HQApp.currencyMap.get(code);
+	public Map<String, Integer> getDiffCurrency() {
+		Map<String, Integer> resultMap = new HashMap<>();
+		int difference = 0;
+		for (Iterator<String> iter = currencyCodes.iterator(); iter.hasNext(); ) {
+			String code = iter.next();
 
-				Integer sumBuyAmount = transactions.stream()
-						.filter(t -> filteredDate.equalsIgnoreCase(String.format("%s", t.getTimeStamp().toLocalDate())))
-						.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
-						.filter(t -> t.getMode().equals(TransactionMode.BUY))
-						.map(t -> t.getAmount())
-						.reduce(0, Integer::sum);
-				
-				Integer sumSellAmount = transactions.stream()
-						.filter(t -> filteredDate.equalsIgnoreCase(String.format("%s", t.getTimeStamp().toLocalDate())))
-						.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
-						.filter(t -> t.getMode().equals(TransactionMode.SELL))
-						.map(t -> t.getAmount())
-						.reduce(0, Integer::sum);
-				
-				Double valueInventory = (sumBuyAmount - sumSellAmount) * temp.getExchangeRate();
-				Double sumOutInSEK = sumBuyAmount * (temp.getExchangeRate()* 0.995);
-				Double sumInInSEK = sumSellAmount * (temp.getExchangeRate()*1.005);
-				
-				int profit = (int) Math.round((sumInInSEK - (sumOutInSEK)) + (valueInventory));
-				
-				resultMap.putIfAbsent(code, profit);
-			}
-			return resultMap;
+			Integer buyAmount = transactions.stream()
+					.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
+					.filter(t -> t.getMode().equals(TransactionMode.BUY))
+					.map(t -> t.getAmount())
+					.reduce(0, Integer::sum);
+
+			Integer sellAmount = transactions.stream()
+					.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
+					.filter(t -> t.getMode().equals(TransactionMode.SELL))
+					.map(t -> t.getAmount())
+					.reduce(0, Integer::sum);
+
+			difference = buyAmount - sellAmount;
+			resultMap.putIfAbsent(code, difference);
 		}
+		return resultMap;
+	}
+
+	public Map<String, Integer> getProfit(String filteredDate) {
+
+		HQApp.currencyMap = HQApp.readCurrencyConfigFile(String.format("ExchangeRates/CurrencyConfig_%s.txt", filteredDate));
+		Map<String, Integer> resultMap = new HashMap<>();
+
+		for (Iterator<String> iter = currencyCodes.iterator(); iter.hasNext(); ) {
+
+			String code = iter.next();
+			Currency temp = HQApp.currencyMap.get(code);
+
+			Integer sumBuyAmount = transactions.stream()
+					.filter(t -> filteredDate.equalsIgnoreCase(String.format("%s", t.getTimeStamp().toLocalDate())))
+					.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
+					.filter(t -> t.getMode().equals(TransactionMode.BUY))
+					.map(t -> t.getAmount())
+					.reduce(0, Integer::sum);
+
+			Integer sumSellAmount = transactions.stream()
+					.filter(t -> filteredDate.equalsIgnoreCase(String.format("%s", t.getTimeStamp().toLocalDate())))
+					.filter(t -> t.getCurrencyCode().equalsIgnoreCase(code))
+					.filter(t -> t.getMode().equals(TransactionMode.SELL))
+					.map(t -> t.getAmount())
+					.reduce(0, Integer::sum);
+
+			Double valueInventory = (sumBuyAmount - sumSellAmount) * temp.getExchangeRate();
+			Double sumOutInSEK = sumBuyAmount * (temp.getExchangeRate()* 0.995);
+			Double sumInInSEK = sumSellAmount * (temp.getExchangeRate()*1.005);
+
+			int profit = (int) Math.round((sumInInSEK - (sumOutInSEK)) + (valueInventory));
+
+			resultMap.putIfAbsent(code, profit);
+		}
+		return resultMap;
+	}
 
 	//	
 	//	public Map<String, Integer> getTransactionCountPerCurrency() {
 	//		
 	//	}
-    
+
 	//	public Map<String, Integer> getAverageAmount() {
 	//		
 	//	}
