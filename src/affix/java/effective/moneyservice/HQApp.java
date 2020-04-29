4b68ed4e479829f92c69852b44491e7ca7c20656
danieelsa <storavarg@gmail.com>
2020-04-29 12:39:17 +0100

    fixed log bug

--- src/affix/java/effective/moneyservice/HQApp.java
+++ src/affix/java/effective/moneyservice/HQApp.java
@@ -136,7 +136,7 @@ public class HQApp {
 			}
 		}
 		catch (IOException ioe) {
-			logger.log(Level.SEVERE, "Could not read CurrencyConfig file properly! " + ioe);
+			logger.log(Level.WARNING, "Could not read CurrencyConfig file properly! " + ioe);
 			System.out.println("An IOException occurred for file ");
 		}
 		logger.info("Exiting readCurrencyConfigFIle method <--");
